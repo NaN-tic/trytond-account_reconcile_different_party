e5391aa103fabccf97a8572db01c25b74f8184f6
resteve <resteve@zikzakmedia.com>
2015-11-29 21:42:04 +0100

    pyflakes

--- tests/test_account_reconcile_different_party.py
+++ tests/test_account_reconcile_different_party.py
@@ -2,18 +2,17 @@
 # This file is part of account_reconcile_different_party module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-import unittest
-import doctest
-import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+import trytond.tests.test_tryton
+import unittest
 
 
 class AccountReconcileDifferentPartyTestCase(unittest.TestCase):
     '''Test Account Reconcile Different Party module'''
 
     def setUp(self):
-        trytond.tests.test_tryton.install_module('account_reconcile_different_party')
+        trytond.tests.test_tryton.install_module(
+            'account_reconcile_different_party')
 
     def test0005views(self):
         '''Test views'''
