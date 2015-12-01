a97b878aeb4f46d5468ccb02fb781ef9701ce781
resteve <resteve@zikzakmedia.com>
2015-12-01 11:02:27 +0100

    Tests

--- tests/test_account_reconcile_different_party.py
+++ tests/test_account_reconcile_different_party.py
@@ -1,26 +1,14 @@
-#!/usr/bin/env python
-# This file is part of account_reconcile_different_party module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
+# This file is part of the account_reconcile_different_party module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountReconcileDifferentPartyTestCase(unittest.TestCase):
-    '''Test Account Reconcile Different Party module'''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'account_reconcile_different_party')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_reconcile_different_party')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class AccountReconcileDifferentPartyTestCase(ModuleTestCase):
+    'Test Account Reconcile Different Party module'
+    module = 'account_reconcile_different_party'
 
 
 def suite():
